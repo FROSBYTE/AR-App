86f53299f5952743df9911b8caff0aacecd4e26b
FROSBYTE <arjunmanoj23@gmail.com>
2023-11-11 00:59:18 +0530

    Added UI Interface

---
+++ Assets/Scripts/UI_Face_Manager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UI_Face_Manager : MonoBehaviour
+{
+    public void Switchto_PlaneDetection()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
