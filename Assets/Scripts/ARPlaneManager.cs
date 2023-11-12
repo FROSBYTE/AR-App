28bec24dd79b8e963271e0b1cf60f7ae41a5f4c6
FROSBYTE <arjunmanoj23@gmail.com>
2023-11-13 00:04:01 +0530

    Plane Detection Demo Almost finished
    Instructions UI needs to be updated
    Improved 3D Models(Animating)

--- Assets/Scripts/ARPlaneManager.cs
+++ Assets/Scripts/ARPlaneManager.cs
@@ -7,16 +7,29 @@ using UnityEngine.XR.ARSubsystems;
 
 public class ARPlaneManager : MonoBehaviour
 {
+    public static ARPlaneManager Instance;
+
     [Header("AR Foundation")]
     [SerializeField] GameObject placementIndicator;
     [SerializeField] ARPlaneManager _arPlaneManager;
     [SerializeField] ARRaycastManager aRRaycastManager;
     [SerializeField] Camera mainARCamera;
+    [SerializeField] GameObject objectToSpawn;
+    [SerializeField] GameObject instructionText;
+
+    private float lastTapTime;
+    public float doubleTapTimeThreshold = 0.5f;
 
     private Pose PlacementPose;
+    private GameObject spawnedObject;
 
     public static bool placementPoseIsValid;
     public static bool isDetected = true;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
 
     void Update()
     {
@@ -24,6 +37,45 @@ public class ARPlaneManager : MonoBehaviour
         {
             UpdatePlacementPose();
             UpdatePlacementIndicator();
+            DoubleTap();
+        }
+    }
+
+    private void DoubleTap()
+    {
+        // Check for touch input
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            // Check if it's a tap
+            if (touch.phase == TouchPhase.Began)
+            {
+                // Check for double tap
+                if (Time.time - lastTapTime < doubleTapTimeThreshold)
+                {
+                    PlaceObject();
+                    // Double tap detected
+                    Debug.Log("Double Tap!");
+                }
+
+                // Update last tap time
+                lastTapTime = Time.time;
+            }
+        }
+        // Check for mouse input
+        else if (Input.GetMouseButtonDown(0))
+        {
+            // Check for double tap
+            if (Time.time - lastTapTime < doubleTapTimeThreshold)
+            {
+                PlaceObject();
+                // Double tap detected
+                Debug.Log("Double Tap!");
+            }
+
+            // Update last tap time
+            lastTapTime = Time.time;
         }
     }
 
@@ -31,8 +83,14 @@ public class ARPlaneManager : MonoBehaviour
     {
         if (placementPoseIsValid)
         {
+            instructionText.SetActive(true);
             placementIndicator.SetActive(true);
             placementIndicator.transform.SetPositionAndRotation(PlacementPose.position, PlacementPose.rotation);
+        }
+        else
+        {
+            placementIndicator.SetActive(false);
+            instructionText.SetActive(false);
         }
     }
 
@@ -50,4 +108,29 @@ public class ARPlaneManager : MonoBehaviour
         }
     }
 
+    public void PlaceObject()
+    {
+        // Check if there is an existing spawnedObject
+        if (spawnedObject != null)
+        {
+            instructionText.SetActive(true);
+            // Destroy the existing spawnedObject
+            Destroy(spawnedObject);
+            spawnedObject = null;
+
+            // Check if enough time has passed since the last instantiation
+            if (Time.time - lastTapTime <= doubleTapTimeThreshold)
+            {
+                return; // Exit the function if it's too soon for a new instantiation
+            }
+        }
+
+        // Instantiate a new objectToSpawn
+        spawnedObject = Instantiate(objectToSpawn, PlacementPose.position, PlacementPose.rotation);
+        instructionText.SetActive(false);
+
+        // Update last tap time
+        lastTapTime = Time.time;
+    }
 }
+
